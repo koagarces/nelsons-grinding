e36908e60a91257e60cdc9561a0fa4dd7f82b2cf
Koa Garces <koajgarces@yahoo.com>
2023-01-02 19:24:47 -0800

    finished surface Grinding page

--- src/pages/BService.jsx
+++ src/pages/BService.jsx
@@ -3,7 +3,48 @@ import "../Styles/Bservices.css";
 const BService = () => {
   return (
     <div className="BServiceBox">
+      <div className="duoImageBox">
+        <img
+          className="duoImage one"
+          src="https://uniim1.shutterfly.com/render/00-MYObA9_qGTLbUvWfy090B4qqNvdh7monO3KLvLrmGrFYuf3BNf0-PuIc-H44Kl33rnHjCyIZu59oV2we_kMG9A?cn=THISLIFE&res=medium&ts=1672629267"
+        />
+        <img
+          className="duoImage two"
+          src="https://uniim1.shutterfly.com/render/00-MYObA9_qGTLbUvWfy090B4qqNvdh7monO3KLvLrmGrG75wI_NHVp5dvKxJoEUxufaAUrgnwgvQnZ_1RWb7NZPA?cn=THISLIFE&res=medium&ts=1672629340"
+        />
+        <img
+          className="duoImage three"
+          src="https://uniim1.shutterfly.com/render/00-MYObA9_qGTLbUvWfy090B4qqNvdh7monO3KLvLrmGrHUom8NttLencLgLmy5VdaAaAUrgnwgvQnZ_1RWb7NZPA?cn=THISLIFE&res=medium&ts=1672177570"
+        />
+        <img
+          className="duoImage three"
+          src="https://uniim1.shutterfly.com/render/00-MYObA9_qGTLbUvWfy090B4qqNvdh7monO3KLvLrmGrFUC8nFUS1E0JYpzV-HgckpaAUrgnwgvQnZ_1RWb7NZPA?cn=THISLIFE&res=medium&ts=1672629768"
+        />
+      </div>
       <h1>Blanchard Grinding (Rotary Surface)</h1>
+      <div>
+        <p>
+          Blanchard grinding (rotary surface) has a large stock removal rate on
+          flat surfaces. Ferrous materials are held on a magnetic chuck while
+          the opposite surface is ground. One side is ground at a time.
+          Non-ferrous materials can also be ground, but require special work
+          holding arrangements. Blanchard Grinding will leave its trademark
+          cross-hatched surface finish pattern, typically within a 32-125 RMS
+          range, though smoother finishes are possible. Dimensional tolerances
+          to +/-.001", parallelism and flatness are achievable on all of our
+          Blanchard Grinders.
+        </p>
+        <p>
+          Because Blanchard Grinders use significantly greater horsepower than
+          other methods, it is ideal for removing large amounts of material in a
+          timely manner. Large parts such as steel plates, castings, and
+          weldments can often be brought to size more economically than other
+          methods of machining. A number of turns may be required to give a part
+          the best flatness by relieving internal stresses equally. Blanchard
+          Grinding eliminates mismatches, and speeds turnaround time on large
+          parts.
+        </p>
+      </div>
       <l className="servicesList">
         <h3 className="service">SERVICES LIST</h3>
         <li className="service">3 - 42" Blanchard Grinders</li>
@@ -13,6 +54,22 @@ const BService = () => {
         <li className="service">
           1 - 24" X 72" Mattison Recipricating Surface Grinder
         </li>
+
+        <li>
+          Tolerances as low as +/- .001 Flatness and parallelism .001 Finishes
+        </li>
+        <li>Flatness and parallelism .001</li>
+        <li>
+          {" "}
+          Finishes to 16 RMS Roughing for subsequent finishing operations
+        </li>
+        <h3>MATERIALS ABLE TO BE BLANCHARD</h3>
+        <p>
+          Aluminum, Castings, Stainless, steel Plate, Weldments, Brass, Copper,
+          Bars, Stampings, Extrusions, Fabricated Assemblies, Titanium, Mold
+          Sets, Plastic, Tapered Configurations, and other Ferrous and
+          Non-Ferrous Metals
+        </p>
       </l>
     </div>
   );
--- src/pages/SGService.jsx
+++ src/pages/SGService.jsx
@@ -1,7 +1,77 @@
-import "../Styles/company.css";
+import "../Styles/SGServices.css";
 
 const SGService = () => {
-  return <div>Surface Service</div>;
+  return (
+    <div className="SGServiceBox">
+      <div className="duoImageBox">
+        <img
+          className="duoImage two"
+          src="https://uniim1.shutterfly.com/render/00-MYObA9_qGTLbUvWfy090B4qqNvdh7monO3KLvLrmGrHKloFA3SNIOoacLo_jA854UnDnu4gHHWpfyYq5WwzmNg?cn=THISLIFE&res=medium&ts=1672291821"
+        />
+        <img
+          className="duoImage three"
+          src="https://uniim1.shutterfly.com/render/00-MYObA9_qGTLbUvWfy090B4qqNvdh7monO3KLvLrmGrGjEyqiwoZ2TPQy4TuggwrZqCe7ffHDJpwno-pEcjtJnw?cn=THISLIFE&res=medium&ts=1672290304"
+        />
+        <img
+          className="duoImage three"
+          src="https://uniim1.shutterfly.com/render/00-MYObA9_qGTLbUvWfy090B4qqNvdh7monO3KLvLrmGrG48k0sbyzqrBwc97dtvMLLaAUrgnwgvQnZ_1RWb7NZPA?cn=THISLIFE&res=medium&ts=1672636337"
+        />
+        <img
+          className="duoImage three"
+          src="https://uniim1.shutterfly.com/render/00-MYObA9_qGTLbUvWfy090B4qqNvdh7monO3KLvLrmGrG3wCyDAapZ7fSuwS34g6L0aAUrgnwgvQnZ_1RWb7NZPA?cn=THISLIFE&res=medium&ts=1672714716"
+        />
+      </div>
+      <h1>Mattison (Surface) Grinding</h1>
+      <div>
+        <p>
+          Recipricating Mattison Grinding is most often the finish operation on
+          parts. Although the surface grinder is not a high stock removal
+          machine, it is much more versatile and can achieve tolerances within
+          tenths of thousandths and surface finishes finer than the Blanchard
+          grinder can produce.
+        </p>
+        <p>
+          On our 24" x 72" Mattison Grinder - Horizontal Spindle Surface we
+          grind heat treated parts to finish dimensions, various size plates
+          flat and parallel and to size within .0002-.0005. The surface grinder
+          is ideal for shear blade sharpening, machined parts such as mold
+          plates, stripper plates, die plates, sub plates, and bolster plates.
+          Mattison Grinding is often used for general manufacturing and tool
+          room requirements such as regrinding or reconditioning of worn machine
+          surfaces. Again, we produce surface ground parts to incredibly close
+          tolerances near .0002, and our production grinders along with our
+          qualified machine operators allow us to grind these surfaces for you
+          and get them right the first time.
+        </p>
+      </div>
+      {/* <l className="servicesList">
+        <h3 className="service">SERVICES LIST</h3>
+        <li className="service">3 - 42" Blanchard Grinders</li>
+        <li className="service">31 - 48" Blanchard Grinder</li>
+        <li className="service">2 - 54" Mattison Rotary Surface Grinders</li>
+        <li className="service">1 - 82" Blanchard Grinder</li>
+        <li className="service">
+          1 - 24" X 72" Mattison Recipricating Surface Grinder
+        </li>
+
+        <li>
+          Tolerances as low as +/- .001 Flatness and parallelism .001 Finishes
+        </li>
+        <li>Flatness and parallelism .001</li>
+        <li>
+          {" "}
+          Finishes to 16 RMS Roughing for subsequent finishing operations
+        </li>
+        <h3>MATERIALS ABLE TO BE BLANCHARD</h3>
+        <p>
+          Aluminum, Castings, Stainless, steel Plate, Weldments, Brass, Copper,
+          Bars, Stampings, Extrusions, Fabricated Assemblies, Titanium, Mold
+          Sets, Plastic, Tapered Configurations, and other Ferrous and
+          Non-Ferrous Metals
+        </p>
+      </l> */}
+    </div>
+  );
 };
 
 export default SGService;
